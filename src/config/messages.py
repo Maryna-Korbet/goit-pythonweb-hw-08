cb9a1d68e2248b95a5334621f189410d8ccc28b9
Maryna-Korbet <m22v86k@gmail.com>
2025-04-12 19:05:51 -0400

    add models, constants, messages

---
+++ src/config/messages.py
@@ -0,0 +1,31 @@
+contact_schema_first_name = {
+    "en": "First name:",
+}
+
+contact_schema_last_name = {
+    "en": "Last name:",
+}
+
+contact_schema_email = {
+    "en": "Email:",
+}
+
+contact_schema_phone = {
+    "en": "Phone:",
+}
+
+contact_schema_birthday = {
+    "en": "Birthday:",
+}   
+
+contact_schema_additional_info = {    
+    "en": "Additional info:",
+}
+
+contact_schema_created_at = {
+    "en": "Created at:",
+}
+
+contact_schema_updated_at = {
+    "en": "Updated at:",
+}
