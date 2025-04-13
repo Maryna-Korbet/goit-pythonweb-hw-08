b0debc75235ec2f4d602e306d44241fc3ed54b89
Maryna-Korbet <m22v86k@gmail.com>
2025-04-13 01:42:10 -0400

    add routes, repository, schemas and services

--- src/config/config.py
+++ src/config/config.py
@@ -1,17 +1,16 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
+from pydantic_settings import BaseSettings
+from pydantic import ConfigDict
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
 class Settings(BaseSettings):
-    DB_URL: str
+    DB_URL: str = os.getenv("DB_URL")
 
-    model_config = SettingsConfigDict(
-        env_file=".env",
-        env_file_encoding="utf-8",
-        case_sensitive=True,
-        extra="ignore"
+    model_config = ConfigDict(
+        env_file=".env", env_file_encoding="utf-8", case_sensitive=True, extra="ignore"
     )
 
 
--- src/entity/models.py
+++ src/entity/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date
 
 from sqlalchemy import String, DateTime, func, Date
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
@@ -18,35 +18,38 @@ class Contact(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     first_name: Mapped[str] = mapped_column(
-        String(constants.FIRST_NAME_MAX_LENGTH), 
+        String(
+            constants.FIRST_NAME_MIN_LENGTH, 
+            constants.FIRST_NAME_MAX_LENGTH
+        ), 
         nullable=False,
         comment=messages.contact_schema_first_name.get('en')
     )
     last_name: Mapped[str] = mapped_column(
-        String(constants.LAST_NAME_MAX_LENGTH), 
+        String(constants.LAST_NAME_MIN_LENGTH, constants.LAST_NAME_MAX_LENGTH), 
         nullable=False,
         comment=messages.contact_schema_last_name.get('en')
     )
     email: Mapped[str] = mapped_column(
-        String(constants.EMAIL_MAX_LENGTH), 
+        String(constants.EMAIL_MIN_LENGTH, constants.EMAIL_MAX_LENGTH), 
         nullable=False,
         unique=True,
         index=True,
         comment=messages.contact_schema_email.get('en')
     )
     phone: Mapped[str | None] = mapped_column(
-        String(constants.PHONE_MAX_LENGTH),
+        String(constants.PHONE_MIN_LENGTH, constants.PHONE_MAX_LENGTH),
         nullable=True,
         comment=messages.contact_schema_phone.get('en')
     )
-    birthday: Mapped[datetime | None] = mapped_column(
+    birthday: Mapped[date | None] = mapped_column(
         Date, 
-        nullable=False,
+        nullable=True,
         comment=messages.contact_schema_birthday.get('en')
     )
     additional_info: Mapped[str | None] = mapped_column(
         String(255), 
-        nullable=False,
+        nullable=True,
         comment=messages.contact_schema_additional_info.get('en')
     )
     created_at: Mapped[datetime] = mapped_column(
